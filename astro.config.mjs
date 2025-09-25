56c1e54b870851497855a35d2dc648d9d597fc84
Karson Lawrence <Karson@thekpsgroup.com>
2025-09-24 22:59:21 -0500

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://modernledger.com',
+  site: 'https://modernledger.co',
   integrations: [
     react(),
     tailwind({
@@ -14,15 +14,15 @@ export default defineConfig({
     mdx(),
     sitemap({
       customPages: [
-        'https://modernledger.com/locations/royse-city-tx',
-        'https://modernledger.com/locations/fate-tx',
-        'https://modernledger.com/locations/rockwall-tx',
-        'https://modernledger.com/locations/caddo-mills-tx',
-        'https://modernledger.com/locations/greenville-tx',
-        'https://modernledger.com/locations/quinlan-tx',
-        'https://modernledger.com/locations/rowlett-tx',
-        'https://modernledger.com/locations/wylie-tx',
-        'https://modernledger.com/locations/heath-tx',
+        'https://modernledger.co/locations/royse-city-tx',
+        'https://modernledger.co/locations/fate-tx',
+        'https://modernledger.co/locations/rockwall-tx',
+        'https://modernledger.co/locations/caddo-mills-tx',
+        'https://modernledger.co/locations/greenville-tx',
+        'https://modernledger.co/locations/quinlan-tx',
+        'https://modernledger.co/locations/rowlett-tx',
+        'https://modernledger.co/locations/wylie-tx',
+        'https://modernledger.co/locations/heath-tx',
       ],
     }),
   ],
